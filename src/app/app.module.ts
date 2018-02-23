9ad6c9f705aff19fc32bbd28eaced8b2c49c54f7
akhila <akhi.prabha@gmail.com>
2018-02-23 16:45:13 +0530

    Integrated Sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AppLayoutComponent } from './app-layout/app-layout.component';
@@ -11,7 +12,10 @@ import { AppLayoutComponent } from './app-layout/app-layout.component';
     AppLayoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
