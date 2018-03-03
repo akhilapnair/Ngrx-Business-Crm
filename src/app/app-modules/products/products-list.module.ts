8e005602135c68afa51f40458b28bd6d29a047fc
daliyajohn <daliyajohn75@gmail.com>
2018-03-03 09:50:56 +0530

    Added products list module

---
+++ src/app/app-modules/products/products-list.module.ts
@@ -0,0 +1,22 @@
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { ProductListComponent } from './components/products-list/products-list.component';
+import { ProductsRoutingModule } from './products-list.routing';
+@NgModule({
+  imports: [
+    FormsModule,
+    CommonModule,
+    HttpModule,
+    ReactiveFormsModule,
+    ProductsRoutingModule,
+  ],
+  providers: [ ],
+  declarations: [
+    ProductListComponent,
+  ]
+})
+
+export class ProductsModule { }
