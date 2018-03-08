cf9c8c82e8c028195a98356c9cf662dfc6884087
daliyajohn <daliyajohn75@gmail.com>
2018-03-08 16:24:27 +0530

    Custom side nav fix

--- src/app/app-layout/app-layout.component.ts
+++ src/app/app-layout/app-layout.component.ts
@@ -9,6 +9,20 @@ import { MatIconModule } from '@angular/material/icon';
 })
 
 export class AppLayoutComponent {
-  showFiller = false;
+  public showMenu = false;
+
+
+
+  sidenav() {
+    this.showMenu = !this.showMenu;
+    document.getElementById('nav').classList.toggle('sideNavExpand');
+    if (this.showMenu === false) {
+      document.getElementById('nav').style.width = '54px';
+      // document.getElementById('nav').classList.add('sideNavNormal');
+    } else {
+      document.getElementById('nav').style.width = '200px';
+      // document.getElementById('nav').classList.add('sideNavExpand');
+    }
+  }
 
 }
