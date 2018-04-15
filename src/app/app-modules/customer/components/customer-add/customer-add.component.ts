2bfc0fbcc7238bdc6069d96b0faf3fe6a13ed4ca
akhila <akhi.prabha@gmail.com>
2018-04-15 12:47:58 +0530

    Customer-list routing fix

---
+++ src/app/app-modules/customer/components/customer-add/customer-add.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-customeradd',
+  templateUrl: './customer-add.component.html',
+  styleUrls: ['./customer-add.component.scss']
+})
+export class CustomerAddComponent {}
--- src/app/app-modules/customer/customer.routing.ts
+++ src/app/app-modules/customer/customer.routing.ts
@@ -3,16 +3,15 @@ import { ModuleWithProviders } from '@angular/core';
 import { NgModule } from '@angular/core';
 
 import { CustomerListComponent } from './components/customer-list/customer-list.component';
+import { CustomerAddComponent } from './components/customer-add/customer-add.component';
 
 const customerRoutes: Routes = [
-  { path: '', redirectTo: 'customer/customerlist', pathMatch: 'full'},
-    { path: 'customerlist', component: CustomerListComponent },
-  ];
-
-  @NgModule({
-    imports: [
-      RouterModule.forChild(customerRoutes)
-    ],
-    exports: [RouterModule]
-  })
-  export class CustomerRoutingModule { }
+  // { path: '', redirectTo: 'customer-add', pathMatch: 'full' },
+  { path: 'customer-list', component: CustomerListComponent },
+  { path: 'customer-add', component: CustomerAddComponent }
+];
+@NgModule({
+  imports: [RouterModule.forChild(customerRoutes)],
+  exports: [RouterModule]
+})
+export class CustomerRoutingModule {}
