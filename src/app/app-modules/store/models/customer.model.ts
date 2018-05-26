13d4c827c4965cef34bab658698b6162f6b8d48d
akhila <akhi.prabha@gmail.com>
2018-05-26 15:32:59 +0530

    customer model

---
+++ src/app/app-modules/store/models/customer.model.ts
@@ -0,0 +1,10 @@
+export interface Customer {
+  address: string;
+  cus_id: string;
+  cust_name: string;
+  email: string;
+  price: string;
+  productdesc: string;
+  prod_id: string;
+  quantity: number;
+}
