1bf4c5d1d8084fe49ec692c11300288db026b3b4
akhila <akhi.prabha@gmail.com>
2018-05-26 15:44:46 +0530

    Add Customer Action

--- src/app/app-modules/store/actions/customer.action.ts
+++ src/app/app-modules/store/actions/customer.action.ts
@@ -1,1 +1,16 @@
 import { Action } from '@ngrx/store';
+import { Customer } from '../models/customer.model';
+
+export const ADD_CUSTOMER = '[CUSTOMER]ADD_CUSTOMER';
+export const ADD_CUSTOMER_SUCCESS = '[CUSTOMER]ADD_CUSTOMER_SUCCESS';
+export const ADD_CUSTOMER_FAIL = '[CUSTOMER]ADD_CUSTOMER_FAIL';
+
+export class AddProperty implements Action {
+  readonly type = ADD_CUSTOMER;
+}
+export class AddPropertySuccess implements Action {
+  readonly type = ADD_CUSTOMER_SUCCESS;
+}
+export class AddPropertyFail implements Action {
+  readonly type = ADD_CUSTOMER_FAIL;
+}
