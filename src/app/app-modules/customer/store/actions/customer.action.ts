1274ba75cc08b87fab664e03034cc6f54a319136
akhila <akhi.prabha@gmail.com>
2018-05-29 15:43:11 +0530

    Customer Load Actions

--- src/app/app-modules/customer/store/actions/customer.action.ts
+++ src/app/app-modules/customer/store/actions/customer.action.ts
@@ -5,12 +5,32 @@ export const ADD_CUSTOMER = '[CUSTOMER]ADD_CUSTOMER';
 export const ADD_CUSTOMER_SUCCESS = '[CUSTOMER]ADD_CUSTOMER_SUCCESS';
 export const ADD_CUSTOMER_FAIL = '[CUSTOMER]ADD_CUSTOMER_FAIL';
 
-export class AddProperty implements Action {
+export const LOAD_CUSTOMER = '[CUSTOMER] LOAD_CUSTOMER';
+export const LOAD_CUSTOMER_SUCCESS = '[CUSTOMER]LOAD_CUSTOMER_SUCCESS';
+export const LOAD_CUSTOMER_FAIL = '[CUSTOMER]LOAD_CUSTOMER_FAIL';
+export class AddCustomer implements Action {
   readonly type = ADD_CUSTOMER;
 }
-export class AddPropertySuccess implements Action {
+export class AddCustomerSuccess implements Action {
   readonly type = ADD_CUSTOMER_SUCCESS;
 }
-export class AddPropertyFail implements Action {
+export class AddCustomerFail implements Action {
   readonly type = ADD_CUSTOMER_FAIL;
 }
+export class LoadCustomer implements Action {
+  readonly type = LOAD_CUSTOMER;
+}
+export class LoadCustomerSuccess implements Action {
+  readonly type = LOAD_CUSTOMER_SUCCESS;
+}
+export class LoadCustomerFail implements Action {
+  readonly type = LOAD_CUSTOMER_FAIL;
+}
+
+export type CustomerAction =
+  | LoadCustomer
+  | LoadCustomerSuccess
+  | LoadCustomerFail
+  | AddCustomer
+  | AddCustomerSuccess
+  | AddCustomerFail;
--- src/app/app-modules/customer/store/reducers/customer.reducer.ts
+++ src/app/app-modules/customer/store/reducers/customer.reducer.ts
@@ -1,1 +1,19 @@
 import * as fromAction from '../actions';
+import { Action } from '@ngrx/store';
+import { Customer } from '../models/customer.model';
+
+export interface CustomerState {
+  customerList: Customer[];
+  loading: boolean;
+}
+
+export const initialState: CustomerState = {
+  customerList: [],
+  loading: false
+};
+
+export function reducers(state= initialState, action: fromAction.) {
+    // switch(){
+
+    // }
+}
