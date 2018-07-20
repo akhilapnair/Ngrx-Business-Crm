4e4b3e147486f14a6b30d7c13055f72089a15bf3
Akhila <akhi.prabha@gmail.com>
2018-07-20 11:21:50 +0530

    interface for login form

--- src/app/app-login/app-login.component.ts
+++ src/app/app-login/app-login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import {Login} from './login.Interface';
 @Component({
   selector: 'app-app-login',
   templateUrl: './app-login.component.html',
@@ -12,12 +12,15 @@ LoginForm:FormGroup;
 
   ngOnInit() {
     this.LoginForm = this.fb.group({
-      username :['',Validators.required],
+      email :['',Validators.required],
       password : ['',Validators.required]
     })
   }
-  login(){
-    
+  login(value){
+    console.log(value);
+    if(value.email == 'akhi.prabha@gmail.com'&&value.password == 'akhila'){
+console.log('Login successfull')
+    }
   }
 
 }
--- src/app/app-login/app-login.module.ts
+++ src/app/app-login/app-login.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'; 
+import { HttpModule } from '@angular/http';
 
 import { AppLoginComponent } from './app-login.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    HttpModule
   ],
   declarations: [AppLoginComponent]
 })
